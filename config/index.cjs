16bb46892d5e315d590bf1086581e3a4b066c375
zzy427 <143980484+zzy427@users.noreply.github.com>
2023-09-04 11:33:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o00aj6pCoLLamNUjEBB6TgPAuE08',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '3F-6ZVvpr9RoqVp4UPaXKEFtuqyOYqSVLojgB7Dt6OY',
+      useTemplateId: '25NUmH0LNmkTHpvo6K5kswYVgIVtpW0Jhz9z22-hLKM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-26',
       festivals: [
@@ -35,14 +35,13 @@ const USER_CONFIG = {
         {
           type: 'birthday1', name: '宝宝', year: '2004', date: '06-26',
         },
-        {
-          type: '节日', name: '相识纪念日', year: '2023', date: '05-27',
-        },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2023-07-08' },
+        // 相识的日子
+        { keyword: 'love_day1', date: '2023-05-27' },
       ],
     },
   ],
@@ -55,7 +54,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '3F-6ZVvpr9RoqVp4UPaXKEFtuqyOYqSVLojgB7Dt6OY',
+      id: '25NUmH0LNmkTHpvo6K5kswYVgIVtpW0Jhz9z22-hLKM',
     }
   ],
 
